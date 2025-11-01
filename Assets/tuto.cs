06455430fd107f21c6420150d31b45742392e78e
Nino lefort <115539009+Ninodev14@users.noreply.github.com>
2025-11-02 00:39:42 +0100

    add tuto

--- Assets/tuto.cs
+++ Assets/tuto.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class tuto : MonoBehaviour
 {
+    public GameObject textToDisppearMoove;
+    public GameObject textToAppear;
+    private PlayerMovement playerMovement;
+    private bool amiPeutApparaitre = true;
+
     // Start is called before the first frame update
     void Start()
     {
+    playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    IEnumerator Wait()
+    {
+        // Attend 2 secondes
+        yield return new WaitForSeconds(2f);
+
+        // Action après le délai
+        Debug.Log("2 secondes se sont écoulées !");
+
+            textToDisppearMoove.SetActive(false);
+            if(amiPeutApparaitre == true){
+                textToAppear.SetActive(true);
+                amiPeutApparaitre = false;
+            }
+            
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!playerMovement.IsMoving())
+            {
+                    StartCoroutine(Wait());
+                    
+            }
     }
+
+    
 }
