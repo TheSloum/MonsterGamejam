6dcad8caa78679c5669251c6ab74f5e8dbf63066
lucasdev30 <lucasbaille30@gmail.com>
2025-11-02 03:45:18 +0100

    dd

--- Assets/Objects/menu/menu.cs
+++ Assets/Objects/menu/menu.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class menu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public PlayerMovement player;
+    public SpriteRenderer startButtonSprite;
+    public SpriteRenderer quitButtonSprite;
+
     void Start()
     {
-        
+        if (player != null)
+            player.canMove = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) // clic gauche
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject == startButtonSprite.gameObject)
+                {
+                    StartGame();
+                }
+                else if (hit.collider.gameObject == quitButtonSprite.gameObject)
+                {
+                    QuitGame();
+                }
+            }
+        }
+    }
+
+    void StartGame()
+    {
+        Debug.Log("▶️ Start cliqué !");
+        if (player != null)
+            player.canMove = true;
+        gameObject.SetActive(false);
+    }
+
+    void QuitGame()
+    {
+        Debug.Log("⛔ Quit cliqué !");
+        Application.Quit();
     }
 }
